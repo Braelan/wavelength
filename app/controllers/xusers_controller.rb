02e248b5a1746af44ab1dcb5e227e6728df51a58
Braelan <bbraelan@gmail.com>
2016-02-06 11:48:37 -0800

    xuser controller change

--- app/controllers/xusers_controller.rb
+++ app/controllers/xusers_controller.rb
@@ -1,7 +1,7 @@
 class XusersController < ApplicationController
   def create
     @xuser = Xuser.create({:zodiac =>params[:zodiac]})
-    render :text "created an xuser"
+    render :text => "created an xuser"
   end
 
   private

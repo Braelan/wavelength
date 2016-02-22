95731eab96c9bef3f0760aeaef2b217dc3c42994
Braelan <bbraelan@gmail.com>
2016-02-21 19:49:41 -0800

    fiddle with database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222005058) do
+ActiveRecord::Schema.define(version: 20160222034333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 20160222005058) do
     t.decimal  "lng"
     t.string   "slide"
     t.string   "video"
-    t.string   "image"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
